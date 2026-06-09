4637715721bcfcbf18450ddd95f1fabba900cb49
eljavi0 <javiermedranohernandez7@gmail.com>
2026-06-09 11:52:53 -0500

    feat:inicio de sesión y registro de usuario aprendiz funcionales
    falta validaciones de formularios y validacion de correo para cerrar el SP-1

---
+++ Data/ProgramaFormacionDAO.cs
@@ -0,0 +1,44 @@
+﻿using EventPlanner.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace EventPlanner.Web.Data
+{
+    public class ProgramaFormacionDAO
+    {
+
+        public List<ProgramaFormacion> Listar()
+        {
+            List<ProgramaFormacion> lista = new List<ProgramaFormacion>();
+
+            Conexion conexion = new Conexion();
+
+            using (SqlConnection cn = conexion.ObtenerConexion())
+            {
+                cn.Open();
+
+                string sql = "SELECT * FROM ProgramaFormacion";
+
+                SqlCommand cmd = new SqlCommand(sql, cn);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    lista.Add(new ProgramaFormacion
+                    {
+                        idProgramaFormacion = (int)dr["idProgramaFormacion"],
+                        NombrePrograma = dr["NombrePrograma"].ToString()
+                    });
+                }
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ Models/ProgramaFormacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventPlanner.Web.Models
+{
+    public class ProgramaFormacion
+    {
+
+        public int idProgramaFormacion { get; set; }
+
+        public string NombrePrograma { get; set; }
+    }
+}
